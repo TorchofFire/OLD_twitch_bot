faa01a4d3b9a0ea1b9a70575b05431d0f9a37099
Torch <mistermiland@gmail.com>
2024-02-05 13:51:08 -0700

    first commit

---
+++ src/objects/character.ts
@@ -0,0 +1,33 @@
+import Matter from 'matter-js';
+
+export default class Character {
+
+    constructor(x: number, y: number, nick: string) {
+        this.username = nick;
+        const characterCollisionGroup = Matter.Body.nextGroup(true);
+        this.body = Matter.Bodies.rectangle(x, y, 40, 100, {
+            restitution: 0,
+            friction: 0,
+            collisionFilter: {
+                group: characterCollisionGroup,
+                category: 0x0002,
+                mask: 0x0001
+            }
+        });
+    }
+
+    username: string;
+
+    onFloor = false;
+
+    actions: {
+        [key: string]: boolean;
+    } = {
+        jump: false,
+        left: false,
+        right: false
+    };
+
+    body: Matter.Body;
+
+}
